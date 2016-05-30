93060c4c8ed87ba1a3c75a4d286f61a50f781e6c
Looo911 <lo.gunreben@gmail.com>
2016-05-30 16:12:20 +0200

    hui, es geht voran. grade gehts mit insert weiter

--- source/Lists.hpp
+++ source/Lists.hpp
@@ -18,6 +18,7 @@ struct ListNode
 	ListNode * m_next;
 };
 
+/*
 template <typename T>
 struct ListIterator
 {
@@ -26,49 +27,152 @@ struct ListIterator
 	private :
 	ListNode <T >* m_node = nullptr ;
 };
+*/
 
 template <typename T>
 struct ListConstIterator
 {
 	friend class List <T >;
-	public :
 	// not implemented yet
 	private :
 	ListNode <T >* m_node = nullptr ;
 };
 
+
+
+
+template <typename T>
+struct ListIterator
+{
+	typedef ListIterator <T> Self;
+	typedef T value_type;
+	typedef T* pointer;
+	typedef T& reference;
+	typedef ptrdiff_t difference_type;
+	typedef std :: forward_iterator_tag iterator_category;
+
+	friend class List <T>;
+
+	ListIterator(): 
+		m_node(nullptr){}
+
+	ListIterator(ListNode <T>*n):
+		m_node(n) {}
+
+	reference operator*() const
+		{return m_node->m_value;}
+
+	pointer operator->() const
+		{return *m_node;}
+
+	Self& operator++() 
+		{
+			m_node = m_node->m_next;
+			return *this;
+		}
+
+	Self operator++(int) 
+	{
+		Self tmp = *this;
+		++(*this);
+		return tmp;
+	}
+
+	bool operator==( const Self & x) const 
+		{return m_node == x.m_node;}
+
+	bool operator!=( const Self & x) const 
+		{return m_node != x.m_node;}
+
+	Self next() const
+	{
+		if (m_node)
+			return ListIterator ( m_node -> m_next );
+		else
+			return ListIterator (nullptr);
+	}
+
+private:
+	// The Node the iterator is pointing to
+	ListNode <T >* m_node = nullptr ;
+};
+
+
+
+template <typename T>
+bool operator==(List<T>const& xs , List<T>const& ys)
+{	
+
+		if (xs.size() == ys.size())
+		{	
+			std::cout <<"Size matches, start comparing:   " << "\n";
+			ListIterator<T> a = ys.begin();
+			ListIterator<T> i;
+			for (i = xs.begin(); i != xs.end(); ++i)
+			{	
+				std::cout << *a <<"   " << *i << "\n";
+				if (*i != *a)
+				{
+					return false;
+				}
+				++a;
+			}
+
+			if(i==xs.end())
+				return true;
+		}
+		return false;
+}
+
+
+template < typename T >
+bool operator !=( List <T > const & xs , List <T > const & ys )
+{
+	return !(xs==ys);
+}
+
+
+
 template <typename T>
 class List
 {
 public:
+	typedef T value_type ;
+	typedef T* pointer ;
+	typedef const T* const_pointer ;
+	typedef T& reference ;
+	typedef const T& const_reference ;
+	typedef ListIterator<T> iterator ;
+	typedef ListConstIterator<T> const_iterator ;
+	friend class ListIterator<T>;
+	friend class ListConstIterator<T>;
+
+
 	List():
 		m_size{0},
 		m_first{nullptr},
 		m_last{nullptr}
 		 {}
-	/*
+	
 	List(List const& a):
+		m_size{0},
 		m_first{nullptr},
-		m_last{nullptr},
-		m_size{0}
+		m_last{nullptr}
+		
 		{
-			for (int i = 0; i < a.size(); ++i)
+			for (auto i = a.begin(); i != a.end(); ++i)
 			{
-				
+				push_back(*i);
 			}
 		}
-	*/
+	
 	std::size_t size() const
-	{
-		//return std::distance(begin(), end());
-		return m_size;
-
-	}
+		{//return m_size;
+			return std::distance(begin(), end());
+		}
 
 	bool empty () const
-	{
-		return size()== 0;
-	}
+		{return size()== 0;}
 
 	void push_front(T const& a)
 	{
@@ -82,10 +186,12 @@ public:
 		else
 		{
 			ListNode<T>* v2 =new ListNode<T>(a, nullptr, m_first);
+			m_first->m_prev = v2;
 			m_first = v2;
 		}
-		m_size=+1;
+		m_size+=1;
 	}
+
 
 	void push_back(T const& a)
 	{
@@ -94,35 +200,102 @@ public:
 			ListNode<T>* v1 =new ListNode<T>(a, nullptr, nullptr);
 			m_first = v1;
 			m_last = v1;
+			
 		}
 		else
 		{
 			ListNode<T>* v3 =new ListNode<T>(a, m_last, nullptr);
+			m_last->m_next = v3;
 			m_last = v3;
 		}
-		
-
-		m_size=+1;
+		m_size+=1;	
 	}
 
+	void pop_front()
+	{	
+		if (empty())
+		{
+        	throw std::out_of_range("Can't pop from empty list");
+		}
 
-	T& front() const{
+		if (m_size == 1)
+		{
+			delete m_first;
+			m_first = nullptr;
+	    	m_last = nullptr;
+	    	m_size -= 1;
+		}
+		
+		else
+		{
+			auto *n = m_first->m_next;
+
+			n->m_prev = nullptr;
+			
+
+			delete m_first;
+			
+			m_first = n;
+
+			m_size -= 1;	
+		}
+	}
+
+	void pop_back()
+	{	
+		if (empty())
+		{
+        throw std::out_of_range("Can't pop from empty list");
+		}
+
+		if (m_size == 1)
+		{
+			delete m_first;
+			m_first = nullptr;
+	    	m_last = nullptr;
+	    	m_size -= 1;
+		}
+		
+		else
+		{
+	        auto *n = m_last->m_prev;
+	        n->m_next = nullptr;
+
+	        delete m_last;
+
+	        m_last = n;
+	        m_size -= 1;
+    	}
+    }
+
+	void clear()
+	{
+		while (!empty())
+	    {	
+		    pop_front();
+	    }           
+    }
+
+
+	T& front() const
+	{
 		return m_first->m_value;
 	}
 
-	T& back() const{
+	T& back() const
+	{
 		return m_last->m_value;
 	}
 
-	typedef T value_type ;
-	typedef T* pointer ;
-	typedef const T* const_pointer ;
-	typedef T& reference ;
-	typedef const T& const_reference ;
-	typedef ListIterator<T> iterator ;
-	typedef ListConstIterator<T> const_iterator ;
-	friend class ListIterator<T>;
-	friend class ListConstIterator<T>;
+	iterator begin() const
+	{
+		return ListIterator<T>(m_first);
+	}
+
+	iterator end() const
+	{
+		return ListIterator<T>();
+	}
 
 
 // not implemented yet
--- source/TestList.cpp
+++ source/TestList.cpp
@@ -21,6 +21,7 @@ TEST_CASE("add an element with push_front", "[modifiers]")
 	list.push_front (42);
 	list.push_front (52);
 	list.push_front (64);
+	REQUIRE(3 == list.size());
 	REQUIRE(64 == list.front());
 	REQUIRE(42 == list.back());
 
@@ -33,10 +34,127 @@ TEST_CASE("add an element with push_back", "[modifiers]")
 	list.push_back (59);
 	list.push_back (52);
 	list.push_back (64);
+	REQUIRE(3 == list.size());
 	REQUIRE(64 == list.back());
 	REQUIRE(59 == list.front());
 
 }
+
+TEST_CASE("delete first and last element with pop", "[modifiers]")
+{	
+
+	List<int> list;
+	list.push_back (1);
+	list.push_back (2);
+	list.push_back (3);
+	list.push_back (4);
+	//list.pop_back();
+	list.pop_front();
+	list.pop_back();
+
+	REQUIRE(2 == list.size());
+	REQUIRE(2 == list.front());
+	REQUIRE(3 == list.back());
+
+	list.pop_front();
+}
+
+TEST_CASE("clear a list", "[modifiers]")
+{	
+
+	List<int> list;
+	list.push_back (1);
+	list.push_back (2);
+	list.push_back (3);
+	list.push_back (4);
+	REQUIRE(list.size() == 4);
+
+
+	list.clear();
+
+	REQUIRE(list.empty() == true);
+
+}
+
+TEST_CASE (" should be an empty range after default construction ","[ iterators ]")
+{
+	List <int > list ;
+	auto b = list.begin();
+	auto e = list.end();
+	REQUIRE (b == e);
+}
+
+TEST_CASE (" provide acces to the first element with begin ", "[ iterators ]") //kein guter Test!
+{
+	List<int> list ;
+	list.push_front (42);
+	REQUIRE (42 == *list.begin());
+}
+
+
+TEST_CASE ("compare two lists", "[operators]")
+{
+	List<int> list;
+	List<int> poo;
+	list.push_back (1);
+	list.push_back (2);
+	list.push_back (3);
+	list.push_back (4);
+	poo.push_back (1);
+	poo.push_back (2);
+	poo.push_back (3);
+	poo.push_back (4);
+	
+	REQUIRE(list == poo);
+	list.pop_front();
+	REQUIRE((list == poo) == false);
+}
+
+TEST_CASE ("compare two lists with different ends", "[operators]")
+{
+	List<int> list;
+	List<int> poo;
+	list.push_back (1);
+	list.push_back (2);
+	list.push_back (3);
+	list.push_back (6);
+	poo.push_back (1);
+	poo.push_back (2);
+	poo.push_back (3);
+	poo.push_back (5);
+	
+	REQUIRE((list == poo) == false);
+
+}
+
+
+TEST_CASE ("copy a list", "[operators]")
+{
+	List<int> list;
+	
+	list.push_back (1);
+	list.push_back (2);
+	list.push_back (3);
+	list.push_back (6);
+	
+	List<int> poo(list);
+	
+	REQUIRE(list == poo);
+
+}
+
+TEST_CASE ( " copy constructor " , " [ constructor ] " )
+{
+	List<int> list ;
+	list.push_front (1);
+	list.push_front (2);
+	list.push_front (3);
+	list.push_front (4);
+	List<int> list2 {list};
+	REQUIRE (list == list2);
+}
+
+
 
 
 int main(int argc, char *argv[])
